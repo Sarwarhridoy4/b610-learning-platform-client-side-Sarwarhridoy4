24e409eecc2c55a4664bffe85a46ae83e81923d3
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-25 14:20:37 +0600

    landing page added and linked with course route

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            <h3>This is Home Component</h3>
+  return (
+    <div>
+      <div
+        className='hero min-h-screen'
+        style={{ backgroundImage: `url("https://i.ibb.co/4P552qJ/freelancer-gab2cdc7af-1280.png")` }}
+      >
+        <div className='hero-overlay bg-opacity-60'></div>
+        <div className='hero-content text-center text-neutral-content'>
+          <div className='max-w-md'>
+            <h1 className='mb-5 text-5xl font-bold'>Welcome Onboard!!</h1>
+            <p className='mb-5'>
+              We are here to help you with Quality online Course. We are always here with 24/7 support. We provide World class course with best trainers.
+            </p>
+            <Link to='/courses'><button className='btn btn-primary'>Get Started</button></Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
