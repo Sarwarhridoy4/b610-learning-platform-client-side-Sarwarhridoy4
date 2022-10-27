8f9dbf4adb67959f109f4d2333f66d1f7b07475a
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-10-27 20:22:42 +0600

    navbar z-index added for mobile device

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -139,7 +139,7 @@ const Navbar = () => {
               </svg>
             </button>
             {isMenuOpen && (
-              <div className='absolute top-0 left-0 w-full'>
+              <div className='absolute top-0 left-0 w-full z-50'>
                 <div className='p-5 bg-white border rounded shadow-sm'>
                   <div className='flex items-center justify-between mb-4'>
                     <div>
